f952c4de030a7ec723bc1121b9ff2e81309d7e39
sneh0102 <ladsneh05@gmail.com>
2024-12-15 19:38:25 +0530

    normal pattern all complete

--- pattern.js
+++ pattern.js
@@ -1,47 +1,110 @@
 // Triangle pattern 
+//       *
+//      * *
+//     *   *
+//    *     *
+//   *       *
+//  *         *
+// *************
 
-
-// function hollowTriangle (n){
-// for (let i = 1; i <= n; i++) {
-//     if(i == 1){
-//         console.log(" ".repeat(n-i) + "*");
-//     }else if(i == n){
-//         console.log("*".repeat(2 * n - 1));
-//     }else{
-//         console.log(" ".repeat(n - i) + "*" + " ".repeat(2 * i - 3) + "*");
-//     }
+function hollowTriangle (n){
+for (let i = 1; i <= n; i++) {
+    if(i == 1){
+        console.log(" ".repeat(n-i) + "*");
+    }else if(i == n){
+        console.log("*".repeat(2 * n - 1));
+    }else{
+        console.log(" ".repeat(n - i) + "*" + " ".repeat(2 * i - 3) + "*");
+    }
     
-// }
-// }
-// hollowTriangle(7)
+}
+}
+hollowTriangle(7)
 
 
 
 
 
-// Square pattern 
+// hollowSquare pattern 
+// *****
+// *   *
+// *   *
+// *   *
+// *****
 
-
-// function hollowSquare (n){
-// for (let i = 1; i <= n; i++) {
+function hollowSquare (n){
+for (let i = 1; i <= n; i++) {
    
-//     if ( i == 1 || i == n){
-//         console.log("*".repeat(n));
-//     }else {
-//         console.log("*" + " ".repeat(n-2) + "*");
-//     }
+    if ( i == 1 || i == n){
+        console.log("*".repeat(n));
+    }else {
+        console.log("*" + " ".repeat(n-2) + "*");
+    }
     
-// }
-// }
-// hollowSquare(5)
+}
+}
+hollowSquare(5)
 
 
 // Square
 
+// * * * * * 
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
 function square (n){
 for (let i = 1; i <= n; i++) {
-
-        console.log("*".repeat(n)); 
+        console.log("* ".repeat(n)); 
 }
 }
 square(5)
+
+
+// Right Triangle
+// * 
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+function rightTriangle (n){
+for (let i = 1; i <= n; i++) {
+        console.log("* ".repeat(i)); 
+}
+}
+rightTriangle(5)
+
+
+// left triangle
+//         * 
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+
+function leftTriangle (n){
+for (let i = 1; i <= n; i++) {
+        console.log("  ".repeat(n-i) + "* ".repeat(i)); 
+}
+}
+leftTriangle(5)
+
+
+
+// full triangle
+//     * 
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+
+function fullTriangle (n){
+for (let i = 1; i <= n; i++) {
+        console.log(" ".repeat(n-i) + "* ".repeat(i)); 
+}
+}
+fullTriangle(5)
