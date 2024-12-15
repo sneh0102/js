ecb38382fc3530dbb2640705ecb33f6ea9d5c9ab
sneh0102 <ladsneh05@gmail.com>
2024-12-15 19:23:09 +0530

    2 new patterns hollow square and simple square

--- pattern.js
+++ pattern.js
@@ -1,14 +1,47 @@
 // Triangle pattern 
-function triangle (n){
-for (let i = 1; i <= n; i++) {
-    if(i == 1){
-        console.log(" ".repeat(n-i) + "*");
-    }else if(i == n){
-        console.log("*".repeat(2 * n - 1));
-    }else{
-        console.log(" ".repeat(n - i) + "*" + " ".repeat(2 * i - 3) + "*");
-    }
+
+
+// function hollowTriangle (n){
+// for (let i = 1; i <= n; i++) {
+//     if(i == 1){
+//         console.log(" ".repeat(n-i) + "*");
+//     }else if(i == n){
+//         console.log("*".repeat(2 * n - 1));
+//     }else{
+//         console.log(" ".repeat(n - i) + "*" + " ".repeat(2 * i - 3) + "*");
+//     }
     
+// }
+// }
+// hollowTriangle(7)
+
+
+
+
+
+// Square pattern 
+
+
+// function hollowSquare (n){
+// for (let i = 1; i <= n; i++) {
+   
+//     if ( i == 1 || i == n){
+//         console.log("*".repeat(n));
+//     }else {
+//         console.log("*" + " ".repeat(n-2) + "*");
+//     }
+    
+// }
+// }
+// hollowSquare(5)
+
+
+// Square
+
+function square (n){
+for (let i = 1; i <= n; i++) {
+
+        console.log("*".repeat(n)); 
 }
 }
-triangle(7)
+square(5)
